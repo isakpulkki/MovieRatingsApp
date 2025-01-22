948d8fa4ccc2801eb89786b1ea83f170fadb1df0
isakpulkki <isak@pulkki.me>
2025-01-22 18:08:20 +0200

    SQLAlchemy fix

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 from flask import Flask
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,11 @@
 from app import app
-from flask import abort, render_template, request, redirect, url_for
+from flask import abort, render_template, request, redirect
 import sql.users as users
 import sql.movies as movies
 import sql.reviews as reviews
 import sql.genres as genres
 import sql.requests as requests
+
 
 @app.route("/", methods=["GET", "POST"])
 def get_movies():
@@ -30,7 +31,7 @@ def search():
         query = request.args["query"]
         if result := movies.search(query):
             return render_template("movies.html", movies=result, genres=genres.get())
-        return render_template("error.html", message="Your search did not match any results")
+        return render_template("notification.html", message="Your search did not match any results.")
 
 
 @app.route("/user", methods=["GET", "POST"])
@@ -38,7 +39,7 @@ def get_user():
     if request.method == "GET":
         if (users.get_user_id()):
             return render_template("user.html")
-        return render_template("error.html", message="You are not logged in")
+        return render_template("notification.html", message="You are not logged in.")
 
     if request.method == "POST":
         genre_id = request.form.get("genre", "all")
@@ -67,7 +68,7 @@ def approve_requests(movieid):
                 requests.delete(movieid)
                 delete_movie(movieid)
             return redirect("/requests")
-        return render_template("error.html", message="You don't have access to this page")
+        return render_template("notification.html", message="You don't have access to this page!")
 
 
 @app.route("/requests", methods=["GET", "POST"])
@@ -77,18 +78,18 @@ def get_requests():
             return (render_template("requests.html", movies=result)
                     if (result := movies.get_requested())
                     else render_template(
-                    "error.html", message="Movies have not been requested yet"))
+                    "notification.html", message="Movies have not been requested yet."))
         else:
-            return render_template("error.html", message="You don't have access to this page")
+            return render_template("notification.html", message="You don't have access to this page")
 
 
 @app.route("/request", methods=["GET", "POST"])
 def request_movie():
     if not users.get_user_id():
-        return render_template("error.html", message="You must be logged in to request new movies")
+        return render_template("notification.html", message="You must be logged in to request new movies!")
     if len(requests.get_user_request()) >= 3:
-        return render_template("error.html", message="You have already requested three movies, wait \
-                                until they are accepted to request a new one")
+        return render_template("notification.html", message="You have already requested three movies, wait \
+                                until they are accepted to request a new one.")
     if request.method == "GET":
         return render_template("request.html", genres=genres.get())
     if request.method == "POST":
@@ -99,25 +100,25 @@ def request_movie():
         genre = request.form["genre"]
         description = request.form["description"]
         if not cover or not description or not name:
-            return render_template("error.html", message="Movie needs name, description and cover photo")
+            return render_template("notification.html", message="Movie needs name, description and cover photo.")
         if len(description) > 980 or len(name) > 180:
-            return render_template("error.html", message="Your name or description is too long")
+            return render_template("notification.html", message="Your name or description is too long.")
         if cover.filename.endswith('.jpg') or cover.filename.endswith('.png') or cover.filename.endswith('.jpeg'):
             data = cover.read()
             if users.get_admin():
                 if not movies.add(name, description, data, genre):
-                    return render_template("error.html", message="Something went wrong")
+                    return render_template("notification.html", message="Something went wrong!")
             elif not requests.add(movies.add(name, description, data, genre)):
-                return render_template("error.html", message="Something went wrong")
-            return redirect("/request")
-        return render_template("error.html", message="Cover photo needs to be in .jpg or .png filetype")
+                return render_template("notification.html", message="Something went wrong!")
+            return render_template("notification.html", message="Movie request submitted! Admin will review it shortly.")
+        return render_template("notification.html", message="Cover photo needs to be in .JPG or .PNG -filetype.")
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add_movie():
     if users.get_admin() and request.method == "GET":
         return render_template("request.html", genres=genres.get())
-    return render_template("error.html", message="You don't have access to this page")
+    return render_template("notification.html", message="You don't have access to this page!")
 
 
 @app.route("/delete/<int:movieid>", methods=["POST"])
@@ -127,7 +128,7 @@ def delete_movie(movieid):
             abort(403)
         if (users.get_admin()) and movies.delete(movieid):
             return redirect("/")
-        return render_template("error.html", message="Something went wrong")
+        return render_template("notification.html", message="Something went wrong!")
 
 
 @app.route("/cover/<int:id>", methods=["GET", "POST"])
@@ -136,7 +137,7 @@ def get_cover(id):
         cover = movies.get_cover(id)
         return (
             cover or render_template(
-                "error.html", message="This cover does not exist"))
+                "notification.html", message="This cover does not exist."))
 
 
 @app.route("/movies/<int:id>", methods=["GET", "POST"])
@@ -145,7 +146,7 @@ def get_movie(id):
         if movies.get_one(id):
             return render_template("movie.html", movie=movies.get_one(id),
                                    reviews=reviews.get_movie_reviews(id), review=reviews.get_user_review(id))
-        return render_template("error.html", message="This movie does not exist")
+        return render_template("notification.html", message="This movie does not exist.")
 
 
 @app.route("/movies/<int:movieid>/reviews/", methods=["POST"])
@@ -156,24 +157,24 @@ def add_review(movieid):
         userid = users.get_user_id()
         if userid:
             if reviews.get_user_review(movieid):
-                return render_template("error.html", message="You have already reviewed this movie")
+                return render_template("notification.html", message="You have already reviewed this movie.")
             if requests.get_movie_request(movieid):
-                return render_template("error.html", message="This movie is still being requested")
+                return render_template("notification.html", message="This movie is still being requested.")
             description = request.form["description"]
             try:
                 stars = int(request.form["rate"])
             except ValueError:
-                return render_template("error.html", message="Something went wrong, did you select a rating?")
+                return render_template("notification.html", message="Something went wrong, did you select a rating?")
 
             if not stars:
-                return render_template("error.html", message="Something went wrong, did you select a rating?")
+                return render_template("notification.html", message="Something went wrong, did you select a rating?")
             if len(description) > 780:
-                return render_template("error.html", message="Review description should be less than \
+                return render_template("notification.html", message="Review description should be less than \
                                        780 characters")
             if stars < 1 or stars > 5 or not reviews.add(description, stars, movieid):
-                return render_template("error.html", message="Something went wrong")
+                return render_template("notification.html", message="Something went wrong")
             return redirect("/movies/" + str(movieid))
-        return render_template("error.html", message="Are you sure you're logged in?")
+        return render_template("notification.html", message="Are you sure you're logged in?")
 
 
 @app.route("/movies/<int:movieid>/reviews/delete/<int:reviewid>", methods=["POST"])
@@ -183,7 +184,7 @@ def delete_review(movieid, reviewid):
     if request.method == "POST":
         if reviews.delete(reviewid):
             return redirect(f"/movies/{str(movieid)}")
-        return render_template("error.html", message="Something went wrong")
+        return render_template("notification.html", message="Something went wrong")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -196,9 +197,9 @@ def login():
         success, value = users.login(username, password)
         if not success:
             if value == 'user':
-                return render_template("error.html", message="No account found with such username")
+                return render_template("notification.html", message="No account found with such username!")
             if value == 'password':
-                return render_template("error.html", message="You did not enter valid password")
+                return render_template("notification.html", message="You did not enter a valid password.")
         return redirect("/")
 
 
@@ -215,17 +216,17 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         if users.username_available(username):
-            return render_template("error.html", message="Username already taken, try another one")
+            return render_template("notification.html", message="Username already taken, try another one.")
         if len(username) < 3 or len(username) > 16:
-            return render_template("error.html", message="Your username should contain 3-16 characters")
+            return render_template("notification.html", message="Your username should contain 3-16 characters.")
         password = request.form["password"]
         password2 = request.form["password2"]
         if password != password2:
-            return render_template("error.html", message="Your password's are not the same")
+            return render_template("notification.html", message="Your password's are not the same.")
         if len(password) < 5 or len(username) > 24:
-            return render_template("error.html", message="Your password should contain 5-24 characters")
+            return render_template("notification.html", message="Your password should contain 5-24 characters.")
         if not users.register(username, password):
-            return render_template("error.html", message="Register failed")
+            return render_template("notification.html", message="Register failed!")
         users.login(username, password)
         return redirect("/")
 
@@ -234,7 +235,7 @@ def register():
 def change_password():
     if request.method == "POST":
         if users.get_admin():
-            return render_template("error.html", message="Admin's password can't be changed this way")
+            return render_template("notification.html", message="Admin's password can't be changed this way.")
         if users.get_user_id():
             if users.check_csrf() != request.form["csrf_token"]:
                 abort(403)
@@ -242,19 +243,21 @@ def change_password():
             password = request.form["password"]
             password2 = request.form["password2"]
             if not password == password2:
-                return render_template("error.html", message="Your password's are not the same")
+                return render_template("notification.html", message="Your password's are not the same!")
             if users.change_password(oldpassword, password):
                 return redirect("/")
-            return render_template("error.html", message="Something went wrong, did you \
+            return render_template("notification.html", message="Something went wrong, did you \
                                    select correct old password?")
 
 
 @app.errorhandler(403)
 @app.errorhandler(404)
 @app.errorhandler(405)
+@app.errorhandler(500)
 def handle_error(error):
     messages = {
-        403: "Something went wrong",
-        404: "The page you are trying to reach does not exist",
-        405: "The method is not allowed for the requested URL"}
-    return render_template('error.html', message=messages.get(error.code))
+        403: "Something went wrong.",
+        404: "The page you are trying to reach does not exist.",
+        405: "The method is not1 allowed for the requested URL.",
+        500: "Internal server error."}
+    return render_template('notification.html', message=messages.get(error.code))
